aa8fa8fe56239eddc905d1cc6f05eb7184aa211d
Kishan <kishan.bagiya1836@gmail.com>
2024-09-29 13:36:47 +0530

    feat: Modules and components are generated & required libraries are installed

---
+++ src/app/pin/pin-list/pin-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pin-list',
+  templateUrl: './pin-list.component.html',
+  styleUrl: './pin-list.component.css'
+})
+export class PinListComponent {
+
+}
---
+++ src/app/pin/pin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PinListComponent } from './pin-list/pin-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    PinListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PinModule { }
